8d6ab9d9ab7bd78ac380c3396dbba52d40a62b9b
ineuwelt <isaac.neuwelt@schalk-and-friends.de>
2023-05-03 16:41:55 +0200

    bug fixes

--- objectLifecycle/StopWatch.java
+++ objectLifecycle/StopWatch.java
@@ -1,32 +1,40 @@
 package objectLifecycle;
 
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class StopWatch {
 
+    long runtime;
+    long start;
+    long end;
 
     public void stopMethod(){
-        Scanner trigger = new Scanner(System.in);
 
-        while(Objects.equals(trigger.nextLine(), "")) {
+        System.out.println("Press Enter to Run Clock:...");
 
-            Date cTime = new Date();
-            Long current = cTime.getTime();
-            System.out.println(current);
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
 
-
-            if(trigger.nextLine() == ""){
-                Long stopTime = cTime.getTime() - current;
-                System.out.println(stopTime);
-                break;
-            }
+        Date t1 = new Date();
+        start = t1.getTime();
+        System.out.println(start);
 
 
+        input = s.nextLine();
+
+        Date t2 = new Date();
+        end = t2.getTime();
+        System.out.println(end);
+
+        runtime = end - start;
+        System.out.println("Runtime = " + runtime);
+
+        DateFormat f1 = DateFormat.getDateInstance(DateFormat.SHORT);
         }
 
 
 //      Date stopTime = Date.from(startTime.toInstant());
     }
-}
